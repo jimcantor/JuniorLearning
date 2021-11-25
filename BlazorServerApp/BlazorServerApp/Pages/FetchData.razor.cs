f11543c52c9f75b3621414351455b7e55f8744cc
Jim Cantor <jim.cantor@interaage.com.au>
2021-11-25 21:01:27 +1100

    ah, forgot to commit!

---
+++ BlazorServerApp/BlazorServerApp/Pages/FetchData.razor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using BlazorServerApp.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerApp.Pages
+    {
+    public partial class FetchData
+        {
+		public WeatherForecast[]? forecasts { get; private set; }
+		[Inject] WeatherForecastService? ForecastService { get; set; } // WeatherForecast [] forecasts; }
+
+        protected override async Task OnInitializedAsync()
+            {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+            }
+        }
+
+    }
